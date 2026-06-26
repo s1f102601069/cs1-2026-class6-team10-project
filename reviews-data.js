95542fa87d1859fdd9e8fde4d00722f31c1efbfb
s1f102603140 <s1f102603140@iniad.org>
2026-06-26 17:48:27 +0900

    細かい修正

--- reviews-data.js
+++ reviews-data.js
@@ -3,7 +3,7 @@ const reviewsData = [
     id: "gameA",
     title: "『ゲームA』レビュー",
     date: "2026年6月",
-    image: "game_review_site_images/87306.jpg",
+    image: "game_review_site_images/スクリーンショット 2026-06-26 174527.png",
     category: "RPG",
     content: "ここにゲームAの詳しい評価や、グラフィック、システムについての詳細な文章"
   },
